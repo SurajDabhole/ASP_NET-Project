9fac86d8d95f1f4c54bdf184e62cf4e88fddcb66
Suraj Dabhole <surajdabhole02@gmail.com>
2025-10-04 22:15:52 +0530

    Enabled CORS for communication between Frontend and Backend running on different ports

--- Backend/Backend/Controllers/PeopleController.cs
+++ Backend/Backend/Controllers/PeopleController.cs
@@ -24,7 +24,7 @@ namespace Backend.Controllers
             {
                 _context.People.Add(person);
                 await _context.SaveChangesAsync();
-                return CreatedAtRoute("GetPerson", new { id = person.Id}, person); // returns 201 Created with the person passed as object in the body, also location of the resource(http://localhost:3000/api/People/{id})
+                return CreatedAtRoute("GetPerson", new { id = person.Id }, person); // returns 201 Created with the person passed as object in the body, also location of the resource(http://localhost:3000/api/People/{id})
             }
             catch (Exception ex)
             {
@@ -49,7 +49,7 @@ namespace Backend.Controllers
 
         // GET: api/People/5
         [HttpGet("{id:int}", Name = "GetPerson")]
-        public async Task<IActionResult> GetPerson(int id )
+        public async Task<IActionResult> GetPerson(int id)
         {
             try
             {
@@ -67,5 +67,59 @@ namespace Backend.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
             }
         }
+
+        // put: api/People/5
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> UpdatePerson(int id, [FromBody] Person person)
+        {
+            try
+            {
+                if (id != person.Id)
+                {
+                    return BadRequest("Person ID mismatch"); // returns 400 Bad Request if the id in the URL does not match the id in the body
+                }
+
+                if (!await _context.People.AnyAsync(p => p.Id == id))
+                {
+                    return NotFound(); // returns 404 Not Found if the person with the specified id does not exist
+                }
+
+                _context.People.Update(person);
+                await _context.SaveChangesAsync();
+                return NoContent(); // returns 204 No Content
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+
+        // DELETE: api/People/5
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeletePerson(int id)
+        {
+            try
+            {
+                var person = await _context.People.FindAsync(id);
+
+                if (person is null)
+                {
+                    return NotFound(); // returns 404 Not Found if the person with the specified id does not exist
+                }
+
+                _context.People.Remove(person);
+                await _context.SaveChangesAsync();
+                //return Ok(person); // returns 200 OK with the deleted person passed as object in the body
+                return NoContent(); // returns 204 No Content
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
     }
 }
+
+
+
--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -1,7 +1,22 @@
 using Backend.Models;
 using Microsoft.EntityFrameworkCore;
 
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Enable CORS for React app running on http://localhost:5173 which is different from the backend url http://localhost:3000
+// for effective communication between frontend and backend, we have to enable CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy  =>
+                      {
+                          policy.WithOrigins("http://localhost:5173") // React app url
+                                .AllowAnyMethod()
+                                .AllowAnyHeader();
+                      });
+});
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -12,6 +27,7 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(connect
 var app = builder.Build();
 
 // Middleware to handle HTTP requests and responses
+app.UseCors(MyAllowSpecificOrigins);
 app.MapControllers();
 
 app.Run();
